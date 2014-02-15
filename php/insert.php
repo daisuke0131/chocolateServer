c379ec3e62ddcb7f4c04cf61ddd4ca00dee35d6c
nagakumi <kumiexe@gmail.com>
2014-02-16 02:07:18 +0900

    no message

--- php/insert.php
+++ php/insert.php
@@ -22,7 +22,27 @@ if(isset($_GET['id']) && isset($_GET['x']) && isset($_GET['y']) && isset($_GET['
   return;
 }
 
-
+$uploaddir = '/var/www/html/chocolate/img/';
+    
+    
+    if ($_FILES["image"]["error"] > 0)
+    {
+        echo "Error: " . $_FILES["image"]["error"] . "<br>";
+    }
+    else
+    {
+        echo "Upload: " . $_FILES["image"]["name"] . "<br>";
+        echo "Type: " . $_FILES["image"]["type"] . "<br>";
+        echo "Size: " . ($_FILES["image"]["size"] / 1024) . " kB<br>";
+        echo "Stored in: " . $_FILES["image"]["tmp_name"];
+        
+        $tmp_name = $_FILES["image"]["tmp_name"];
+        $name     = $_FILES["image"]["name"];
+        echo "tmp_name:" . $tmp_name;
+        echo "name    :" . $name;
+    }
+    
+    
 $db_server = mysql_connect($db_hostname, $db_username, $db_password);
 
 if (!$db_server)
