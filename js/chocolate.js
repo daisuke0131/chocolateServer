d890e8765b76651e735d8e122bfa0d130f740602
sumioka <sumioka.motoshi@gmail.com>
2014-02-15 23:11:44 +0900

    cheerio対応

--- js/chocolate.js
+++ js/chocolate.js
@@ -1,14 +1,17 @@
 var cheerio = require("cheerio");
-
+var formbar = "aaaaaaa";
+/*	
+"<form method='POST' enctype='multipart/form-data' action='http://www1034up.sakura.ne.jp/chocolate/php/register_image.php'>
+<input type='hidden' name='url' id='form_url' value=''>
+<input type='file' name='image'></input>
+<input type='submit' value='“o˜^'></input>
+</form>";
+*/
 module.exports = {
 
 	chocolatify : function(body){
-    $ = cheerio.load('<h2 class="title">Hello world</h2>');
-
-	$('h2.title').text('Hello there!');
-	$('h2').addClass('welcome');
-
-	$.html();
+	    $ = cheerio.load(body);
+		$('body').prepend(formbar);
 		return body;
 	}
 }
